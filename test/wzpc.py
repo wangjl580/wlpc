7d6525b934819e1c9cd48677bf58c3bceafbca5a
wangjinlong <396292346@qq.com>
2021-10-15 23:59:09 +0800

    1

---
+++ test/wzpc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#---网站爬虫
+# -*- coding: UTF-8 -*-
+from urllib import request
+response = request.urlopen("http://www.baidu.com/")  # 打开网站
+fi = open("project.txt", 'w')                        # open一个txt文件
+page = fi.write(str(response.read()))                # 网站代码写入
+fi.close()                                           # 关闭txt文件
+
